71f242ac1ac19748c68b446845974ec702472c0a
ktopchiev <karlostopchiev@gmail.com>
2022-09-27 21:27:56 +0300

    added new controller Products, model ProductView and new View for All products

---
+++ ASP.NET_Fundamentals/SimplePage/SimplePage/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePage.Models;
+
+namespace SimplePage.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductsViewModel> products =
+            new List<ProductsViewModel>()
+            {
+                new ProductsViewModel
+                {
+                    Id = 1,
+                    Name = "Cheese",
+                    Price = 7.00
+                },
+                new ProductsViewModel
+                {
+                    Id = 2,
+                    Name = "Ham",
+                    Price = 5.50
+                },
+                new ProductsViewModel
+                {
+                    Id = 3,
+                    Name = "Bread",
+                    Price = 1.50
+                }
+            };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            return View(this.products);
+        }
+    }
+}
---
+++ ASP.NET_Fundamentals/SimplePage/SimplePage/Models/ProductsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SimplePage.Models
+{
+    public class ProductsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
