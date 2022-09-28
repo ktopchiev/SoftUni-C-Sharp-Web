99fea3c22f2fcc0f079015b30fa560c3035796eb
ktopchiev <karlostopchiev@gmail.com>
2022-09-28 18:06:40 +0300

    Added ById page where we can get product by id

--- ASP.NET_Fundamentals/SimplePage/SimplePage/Controllers/ProductsController.cs
+++ ASP.NET_Fundamentals/SimplePage/SimplePage/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SimplePage.Models;
+using System.Text.Json;
 
 namespace SimplePage.Controllers
 {
@@ -35,6 +36,41 @@ namespace SimplePage.Controllers
         public IActionResult All()
         {
             return View(this.products);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            return Json(products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            string text = string.Empty;
+
+            foreach (var pr in this.products)
+            {
+                text += $"Product 1: {pr.Name} - {pr.Price}lv.";
+                text += "\r\n";
+            }
+
+            return Content(text);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = this.products.FirstOrDefault(i => i.Id == id);
+
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            return View(product);
         }
     }
 }
