e77c687c33788264e1d65d6d70bd1e4234455b70
ktopchiev <karlostopchiev@gmail.com>
2022-10-03 19:30:22 +0300

    Added validation to text field

--- ASP.NET_Fundamentals/TextSplitter/TextSplitter/Controllers/HomeController.cs
+++ ASP.NET_Fundamentals/TextSplitter/TextSplitter/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
 using TextSplitter.Models;
 
 namespace TextSplitter.Controllers
@@ -13,14 +14,25 @@ namespace TextSplitter.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(TextViewModel model)
         {
-            return View();
+            return View(model);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult SplitText(TextViewModel model)
+        {
+            string[] result = model.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplittedText = String.Join(Environment.NewLine, result);
+
+            return RedirectToAction("Index", model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
