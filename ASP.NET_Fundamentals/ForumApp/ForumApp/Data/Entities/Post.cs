d273a52ab2f2a2924d304b04c9bfd02e092e6e19
ktopchiev <karlostopchiev@gmail.com>
2022-10-17 20:11:43 +0300

    Initialize the app

---
+++ ASP.NET_Fundamentals/ForumApp/ForumApp/Data/Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ForumApp.Data.DataConstants.Post;
+
+namespace ForumApp.Data.Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(TitleMaxLength)]
+    public string Title { get; set; }
+    
+    [Required]
+    [MaxLength(ContentMaxLength)]
+    public string Content { get; set; }
+}
---
+++ ASP.NET_Fundamentals/ForumApp/ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+using Microsoft.EntityFrameworkCore;
+using ForumApp.Data.Entities;
+
+namespace ForumApp.Data;
+
+public class ForumAppDbContext : DbContext
+{
+    public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+        :base(options)
+    {
+        this.Database.Migrate();
+    }
+    
+    public DbSet<Post> Posts { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Post>();
+        
+        base.OnModelCreating(builder);
+    }
+}
