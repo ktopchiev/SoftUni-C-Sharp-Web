a00b06186c0a9f3c063db4a2097fc29daaaa71d3
ktopchiev <karlostopchiev@gmail.com>
2022-10-22 11:57:03 +0300

    added funcitonality for create tasks

---
+++ ASP.NET_Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/TasksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using TaskBoardApp.Data;
+using TaskBoardApp.Models.Tasks;
+using TaskBoardApp.Data.Entities;
+using Task = TaskBoardApp.Data.Entities.Task;
+using System.Security.Claims;
+
+namespace TaskBoardApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly TaskBoardAppDbContext data;
+
+        public TasksController(TaskBoardAppDbContext context)
+        {
+            data = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            TaskFormModel taskModel = new TaskFormModel()
+            {
+                Boards = GetBoards()
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskFormModel taskModel)
+        {
+            if (!GetBoards().Any(b => b.Id == taskModel.BoardId))
+            {
+                this.ModelState.AddModelError(nameof(taskModel.BoardId), "Board does not exist.");
+            }
+
+            string currentUserId = GetUserId();
+            Task task = new Task()
+            {
+                Title = taskModel.Title,
+                Description = taskModel.Description,
+                CreatedOn = DateTime.Now,
+                BoardId = taskModel.BoardId,
+                OwnerId = currentUserId
+            };
+
+            this.data.Tasks.Add(task);
+            this.data.SaveChanges();
+
+            var boards = this.data.Boards;
+
+            return RedirectToAction("All", "Boards");
+        }
+
+        private IEnumerable<TaskBoardModel> GetBoards()
+        {
+            return this.data.Boards
+                .Select(b => new TaskBoardModel()
+                {
+                    Id = b.Id,
+                    Name = b.Name
+                });
+        }
+
+        private string GetUserId() => this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
