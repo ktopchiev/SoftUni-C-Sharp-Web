93066d83daff91994e7567f5b6d86ab6afbbcb0a
ktopchiev <karlostopchiev@gmail.com>
2022-10-22 13:37:35 +0300

    added delete action and view

--- ASP.NET_Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/TasksController.cs
+++ ASP.NET_Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/TasksController.cs
@@ -144,5 +144,50 @@ namespace TaskBoardApp.Controllers
 
             return RedirectToAction("All", "Boards");
         }
+
+        public IActionResult Delete(int id)
+        {
+            Task task = data.Tasks.Find(id);
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            TaskViewModel taskModel = new TaskViewModel()
+            {
+                Id = task.Id,
+                Title = task.Title,
+                Description = task.Description
+            };
+
+            return View(taskModel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(TaskViewModel taskModel)
+        {
+            Task task = data.Tasks.Find(taskModel.Id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+            if (currentUserId != task.OwnerId)
+            {
+                return Unauthorized();
+            }
+
+            this.data.Tasks.Remove(task);
+            this.data.SaveChanges();
+            return RedirectToAction("All", "Boards");
+        }
     }
 }
